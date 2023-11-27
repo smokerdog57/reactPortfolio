1fc87913becab07ba8695db25d3142d16cd8f4ac
Tom Fusco <smokerdog57@gmail.com>
2023-11-27 04:42:20 -0500

    v0.3 refined header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,20 +7,12 @@ const Header = () => {
   return (
     <header>
       <h1>Tom Fusco</h1>
-      <ul>
-        <li>
-          <a href="#about-me">About Me</a>
-        </li>
-        <li>
-          <a href="#portfolio">Portfolio</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-        <li>
-          <a href="#resume">Resume</a>
-        </li>
-      </ul>
+      <nav>
+        <a href="#about-me">About Me</a>
+        <a href="#portfolio">Portfolio</a>
+        <a href="#contact">Contact</a>
+        <a href="#resume">Resume</a>
+      </nav>
     </header>
   );
 }
