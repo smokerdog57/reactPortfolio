6155e3868bd34ce95c5c150147299a5929104208
Tom Fusco <smokerdog57@gmail.com>
2023-11-27 05:04:01 -0500

    v0.4 removed default VITE/REACT elements

--- src/App.jsx
+++ src/App.jsx
@@ -12,28 +12,11 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <Header /> {/* Make sure this line is present */}
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        <p>HELLO WORLD</p> {/* Add this line */}
+      <div className="card">      
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
