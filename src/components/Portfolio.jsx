22d09c9351b59b957a1c81f5f1c02162934dc92e
Tom Fusco <smokerdog57@gmail.com>
2023-11-29 10:47:30 -0500

    v0.9 created reusable Project.jsx component

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,15 +1,39 @@
 // /src/components/Portfolio.jsx
 
-// import dependencies
 import React from 'react';
+import Project from './Project';
 
 const Portfolio = () => {
+  // Replace the dummy data with your actual project data
+  const projects = [
+    {
+      title: 'Horiseon',
+      imageSrc: '../../images/digital-marketing-meeting.jpg',
+      deployedLink: 'https://smokerdog57.github.io/refactoring/',
+      githubLink: 'https://github.com/smokerdog57/portfolio',
+    },
+    {
+      title: 'WeatherApp',
+      imageSrc: '../../images/landingpage.png',
+      deployedLink: 'https://smokerdog57.github.io/weatherdashboard/',
+      githubLink:  'https://github.com/smokerdog57/weatherdashboard/',
+    },
+    {
+      title: 'Your Space',
+      imageSrc: '../../images/Spaceman.PNG',
+      deployedLink: 'https://your-space-b0e277e1dff1.herokuapp.com/home/',
+      githubLink:  'https://github.com/alejandra-e-bernabel/YourSpace',
+    },
+    // Add more projects as needed
+  ];
+
   return (
     <div>
-      <h2>Portfolio</h2>
-      <p>Portfolio will display here ...</p>
+      {projects.map((project, index) => (
+        <Project key={index} {...project} />
+      ))}
     </div>
   );
-}
+};
 
 export default Portfolio;
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,61 +2,26 @@
 
 // import dependencies
 import React from 'react';
-import { FaGithub, FaLinkedin, FaEnvelope, FaPhone } from 'react-icons/fa';
 
-
-const Project = () => {
+// reusable project component
+const Project = ({ title, imageSrc, deployedLink, githubLink }) => {
   return (
-    <main>
-      <section id="aboutme" className="section_topic">
-        <h2>About Me</h2>
-        <p>Mr Fusco has a batchelor degree ...</p>
-      </section>
-      <section id="work" className="section_topic">
-        <h2>Portfolio</h2>
-
-        {/* Horiseon project w/ live link */}
-        <section className="card">
-          <a href="https://smokerdog57.github.io/refactoring/" target="__blank">
-            <header>Horiseon</header>
-            <img src="../../images/digital-marketing-meeting.jpg" alt="Tablet" />
+    <div id = "work" className = "section_topic">
+      <h2>Portfolio</h2>
+      <div className="card">
+        <h3>{title}</h3>
+        <img src={imageSrc} alt={`Screenshot of ${title}`} />
+        <div>
+          <a href={deployedLink} target="_blank" rel="noopener noreferrer">
+            Deployed App
           </a>
-        </section>
-
-        {/* weather dashboard project w/ live link */}
-        <section className="card">
-          <a href="https://smokerdog57.github.io/weatherdashboard/" target="_blank">
-            <header>WeatherApp</header>
-            <img src="../../images/landingpage.png" alt="WeatherApp" />
+          <a href={githubLink} target="_blank" rel="noopener noreferrer">
+            GitHub Repository
           </a>
-        </section>
-
-        {/* your space social network project w/ live link */}
-        <section className="card">
-          <a href="https://your-space-b0e277e1dff1.herokuapp.com/home">
-            <header>Your Space</header>
-            <img src="../../images/Spaceman.PNG" alt="Picture of a Spaceman" />
-          </a>
-        </section>
-      </section>
-
-      {/* contact info */}
-      <section id="contactme" className="section_topic">
-        <h2>Contact Me</h2>
-        <p>
-          <a href="mailto:smokerdog57@gmail.com">
-            <FaEnvelope size={30} />
-          </a>
-          <a href="https://github.com/smokerdog57/reactPortfolio" target="_blank" rel="noopener noreferrer">
-            <FaGithub size={30} />
-          </a>
-          <a href="https://linkedin.com/in/tom-fusco/" target="_blank" rel="noopener noreferrer">
-            <FaLinkedin size={30} />
-          </a>
-        </p>
-      </section>
-    </main >
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default Project;
