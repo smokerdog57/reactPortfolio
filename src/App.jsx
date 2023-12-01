9513d566d5752d1a3bdbe6e2cded348f6bd9e01d
Tom Fusco <smokerdog57@gmail.com>
2023-12-01 04:53:00 -0500

    v.92 working on flex styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-// App.js
-import React from 'react';
-import FlexboxComponent from '../src/components/FlexboxComponent';
+// /src/App.jsx
 
-function App() {
+// import dependencies
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AboutMe from './components/AboutMe';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import './styles/app.css';
+
+const App = () => {
   return (
-    <div>
-      <FlexboxComponent />
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<AboutMe />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -7,19 +7,17 @@ import '../styles/project.css';
 // reusable project component
 const Project = ({ title, imageSrc, deployedLink, githubLink }) => {
   return (
-    <div className = "card_container">
-      <div className="project">
-        <div className="project-info">
-          <img src={imageSrc} alt={`Screenshot of ${title}`} className="project-image" />
-          <div className="project-overlay">
-            <a href={deployedLink} target="_blank" rel="noopener noreferrer" className="project-title">
-              {title}
+    <div className="project-outer-container">
+      <div className="project-card-flex-container">
+        <img src={imageSrc} alt={`Screenshot of ${title}`} className="project-image" />
+        <div className="project-card-overlay">
+          <a href={deployedLink} target="_blank" rel="noopener noreferrer" className="project-title">
+            {title}
+          </a>
+          <div className="project-links">
+            <a href={githubLink} target="_blank" rel="noopener noreferrer">
+              <span className="link-text">GitHub Repository</span>
             </a>
-            <div className="project-links">
-              <a href={githubLink} target="_blank" rel="noopener noreferrer">
-                <span className="link-text">GitHub Repository</span>
-              </a>
-            </div>
           </div>
         </div>
       </div>
