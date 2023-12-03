82099dad5263334b6d25d77b2a9022d6ced76e13
Tom Fusco <smokerdog57@gmail.com>
2023-12-03 08:18:47 -0500

    v.94 working on contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,89 @@
-// /src/components/Contact.jsx
+// /src/components/contact.jsx v0.94
 
 // import dependencies
-import React from 'react';
+import React, { useState } from 'react';
+import '../styles/contact.css';
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Check if all fields are not blank
+    if (!formData.name || !formData.email || !formData.message) {
+      alert('Please fill in all fields.');
+      return;
+    }
+
+    // Check if the email is valid
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(formData.email)) {
+      alert('Please enter a valid email address.');
+      return;
+    }
+
+    // If all checks pass, you can proceed with submitting the form or other actions
+    // For now, let's just log the form data to the console
+    console.log('Form submitted:', formData);
+  };
+
   return (
-    <div>
-      <h2>Contact</h2>
-      <p>Place contact form here ...</p>
+    <div className="contact-container">
+      <form className="contact-form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="email">Email address:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            pattern="^[^\s@]+@[^\s@]+\.[^\s@]+$" // Email validation pattern
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="message">Message:</label>
+          <textarea
+            id="message"
+            name="message"
+            rows="4"
+            value={formData.message}
+            onChange={handleInputChange}
+          ></textarea>
+        </div>
+
+        <button type="submit" className="submit-button">
+          Submit
+        </button>
+      </form>
     </div>
   );
-}
+};
 
 export default Contact;
